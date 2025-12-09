f562b5e9b758c183682a7887479392d260d99f61
DangHieu19022004 <danghieu19122004@gmail.com>
2025-12-09 12:57:15 +0700

    fix: update unit code mapper old

--- frontend/src/utils/unitCodeMapper.js
+++ frontend/src/utils/unitCodeMapper.js
@@ -14,6 +14,8 @@
 // models, MQTT-based data ingestion, and FiWARE Smart Data Models for
 // open-data services and smart-city applications.
 
+
+
 /**
  * UN/CEFACT Common Code to Human-Readable Unit Mapper
  * 
